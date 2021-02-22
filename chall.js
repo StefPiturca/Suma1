d8582a4708c70a1180f51f0af61f432ac63b1064
StefPiturca <74204702+StefPiturca@users.noreply.github.com>
2021-02-22 14:49:53 +0000

    Add files via upload

---
+++ chall.js
@@ -0,0 +1,18 @@
+let prime=true
+var x=7;
+var t=4;
+function isprime(t) {
+    if (t<2)
+    return false;
+    if (t==2)
+    return true;
+    var Maxdiv = Math.Sqrt(t);
+} 
+for (var i=0;i<2;i++) { 
+     if (t % i==0) {
+       isprime==false;
+     }
+     isprime===true;
+    }
+    console.log(isprime(2))
+
